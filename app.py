819e15e7a3a4f3e9036ca3b8e5f382bae4a5ca70
lucebv96 <barretolucero.lb@gmail.com>
2024-06-12 11:40:55 -0400

    1

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #Tabla de Multiplicar: Escribe un programa que muestre la tabla de multiplicar de un número dado por el usuario.
 
-#Se define 'n' como el numero que definira la tabla de multiplicar que desea ver el usuario
+#Se define 'n' como el numero que definira la tabla de multiplicar que desea ver el usuario :)
 n = int(input("La tabla de que numero desea visualizar? "))
 
 #Se inicializa las variables
